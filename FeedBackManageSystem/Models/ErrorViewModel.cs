0221dd4c3146847082e185c570814f574db4bee7
Vaibhav <vaibhav@2020i.com>
2025-12-23 16:54:20 +0530

    Add project files.

---
+++ FeedBackManageSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FeedBackManageSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
