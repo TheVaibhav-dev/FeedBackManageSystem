1fcb258022a9050c6885f4198ac07527d161f119
Vaibhav <vaibhav@2020i.com>
2025-12-25 11:20:03 +0530

    Day 1 code commit with feedback Create Module and Read module .

---
+++ FeedBackManageSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FeedBackManageSystem.Models;
+
+namespace FeedBackManageSystem.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<FeedbackViewModel> tblFeedback { get; set; }
+    }
+}
