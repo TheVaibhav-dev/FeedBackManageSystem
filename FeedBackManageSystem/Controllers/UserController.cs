af11487592bdfe82a3e491ba1cf8eb6cf22d9e14
vsrivastava943@gmail.com <vsrivastava943@gmail.com>
2026-02-22 18:36:52 +0530

    code commit 22-02-2026

--- FeedBackManageSystem/Controllers/UserController.cs
+++ FeedBackManageSystem/Controllers/UserController.cs
@@ -78,7 +78,11 @@ namespace FeedBackManageSystem.Controllers
         }
         public IActionResult Logout() 
         {
-            HttpContext.Session.Clear();
+            ProjectSession.Clear();
+            Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+            Response.Headers["Pragma"] = "no-cache";
+            Response.Headers["Expires"] = "0";
+
             return RedirectToAction("MyVisitor", "Visitor");
         }
     }
